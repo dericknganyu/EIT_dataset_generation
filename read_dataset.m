177a59b20e4348906e04305605f429ecbcda3e15
@GL_TanyuD <derick@uni-bremen.de>
2023-08-03 18:00:46 +0200

    file to read the dataset in the subdirectories of dataset directory

---
+++ read_dataset.m
@@ -0,0 +1,18 @@
+path = '../dataset';
+
+data_boundary = read('bound.mat');
+data_domain   = read('bound.mat');
+mesh          = read('mesh.mat');
+
+function [data] = read(path, ending)
+    dir_list = dir(path);
+    for i = 1:numel(dir_list)
+        file = dir_list(i).name;
+        if ~dir_list(i).isdir && endsWith(file, ending)
+            file_path = fullfile(path, file);
+            % Process the file
+            data = load(file_path);
+        end
+    end
+end
+
