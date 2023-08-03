d19f538f612f01c4e854025c8db837b81ad2e418
@GL_TanyuD <derick@uni-bremen.de>
2023-08-03 18:08:26 +0200

    made it a function file

--- read_dataset.m
+++ read_dataset.m
@@ -1,10 +1,11 @@
-path = '../dataset';
+% #Usage#
+% path = '../dataset';
 
-data_boundary = read('bound.mat');
-data_domain   = read('bound.mat');
-mesh          = read('mesh.mat');
+% data_boundary = read('bound.mat');
+% data_domain   = read('bound.mat');
+% mesh          = read('mesh.mat');
 
-function [data] = read(path, ending)
+function [data] = read_dataset(path, ending)
     dir_list = dir(path);
     for i = 1:numel(dir_list)
         file = dir_list(i).name;
